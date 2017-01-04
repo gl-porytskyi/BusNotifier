599eb67cdc3b6b36cb8be6ad1c862170054d766b
oleksandr.porytskyi <oleksandr.porytskyi@globallogic.com>
2017-01-04 16:30:43 +0200

    Add notification removing

--- app/src/main/java/oporytskyi/busnotifier/manager/MessageManager.java
+++ app/src/main/java/oporytskyi/busnotifier/manager/MessageManager.java
@@ -1,6 +1,9 @@
 package oporytskyi.busnotifier.manager;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 import android.app.Application;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
@@ -10,35 +13,36 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-
 /**
  * @author Oleksandr Porytskyi
  */
 public class MessageManager {
+    private final int mNotificationId = 1;
+
     private Application application;
+    private NotificationManager notificationManager;
 
     public MessageManager(Application application) {
         this.application = application;
+        notificationManager = (NotificationManager) application.getSystemService(NOTIFICATION_SERVICE);
     }
 
     public void showNotification(DateTime dateTime, String directionName) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormat.shortTime();
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(application)
-                .setSmallIcon(R.drawable.ic_dialog_time)
-                .setContentTitle(directionName)
-                .setContentText("Departure at " + dateTime.toString(dateTimeFormatter));
+            .setSmallIcon(R.drawable.ic_dialog_time)
+            .setContentTitle(directionName)
+            .setContentText("Departure at " + dateTime.toString(dateTimeFormatter));
 
         Intent intent = new Intent(application, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(application, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(pendingIntent);
         mBuilder.setOngoing(true);
 
-        int mNotificationId = 1;
-        android.app.NotificationManager notificationManager = (android.app.NotificationManager) application.getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(mNotificationId, mBuilder.build());
     }
 
     public void remove() {
+        notificationManager.cancel(mNotificationId);
     }
 }
